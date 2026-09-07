ecd5c139c788b8f1b4abaf696528863626577871
danielcarlmiguell-gif <danielcarlmiguell@gmail.com>
2026-09-07 11:26:04 +0800

    Intro to Constructor and Properties

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -13,20 +13,9 @@ namespace BattleArena
     {
         static void Main(string[] args)
         {
-            Warrior Raymond = new Warrior();
-            Raymond.Name = "Raymond";
-            Raymond.AttackPower = 30;
-            Raymond.Health = 100;
-
-            Warrior Kirk = new Warrior();
-            Kirk.Name = "Kirk";
-            Kirk.AttackPower = 15;
-            Kirk.Health = 200;
-
-            Warrior Jibi = new Warrior();
-            Jibi.Name = "Jibi";
-            Jibi.AttackPower = 20;
-            Jibi.Health = 150;
+            Warrior Raymond = new Warrior("Raymond", 100, 30);
+            Warrior Kirk = new Warrior("Kirk", 200, 15);
+            Warrior Jibi = new Warrior("Jibi", 150, 20);
 
             Console.WriteLine($"{Raymond.Name} has " + 
                 $"{Raymond.Health} health and {Raymond.AttackPower} " + $"attack power.");
@@ -37,6 +26,7 @@ namespace BattleArena
             Console.WriteLine($"{Jibi.Name} has " +
                 $"{Jibi.Health} health and {Jibi.AttackPower} attack power.");
             Console.WriteLine("-----------------------------------");
+
             Console.ReadKey();
         }
     }
